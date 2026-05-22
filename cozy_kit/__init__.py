f76e31cbdccf758bf3fa90cd6f0bde1f051ca9f3
youssefahmed2017 <youssef.ahmed.29062017@gmail.com>
2026-05-22 23:37:02 +0300

    Add files via upload

---
+++ cozy_kit/__init__.py
@@ -0,0 +1,25 @@
+import importlib.metadata as lib
+
+from .greeting import Greeting
+from .timer import Timer
+from .text_studio import TextEditor
+from .text_studio import TextCustomizations
+from .details import Details
+from .ui import CozyUI
+from .updater import check_for_updates
+
+check_for_updates()
+
+__version__ = lib.version("cozy-kit")
+__version_info__ = tuple(map(int, __version__.split(".")))
+
+__all__ = [
+    "Greeting",
+    "Timer",
+    "TextEditor",
+    "Details",
+    "CozyUI",
+    "__version__",
+    "__version_info__",
+    "TextCustomizations",
+]
